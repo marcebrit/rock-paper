9e65cb8bc41ae1af9869ea00f89314c455d6c8ff
marcebrit <248681894+marcebrit@users.noreply.github.com>
2025-12-23 02:35:31 -0300

    Now the counter shows both scores, not only the player's score

--- script.js
+++ script.js
@@ -6,82 +6,85 @@ let playerScore = 0
 let computerScore = 0
 
 
-function fullGame (){
+function fullGame() {
 
-function gethumanChoice() {
-    let userInput = (prompt("rock, paper or scissors"))
+    function gethumanChoice() {
+        let userInput = (prompt("rock, paper or scissors"))
 
-    if (userInput == "rock") {
-        return "rock"
-    }
-
-    else if (userInput == "paper") {
-        return "paper"
-
-    }
-    else if (userInput == "scissors") {
-        return "scissors"
-    }
-
-    else {
-        return "error"
-    }
-
-}
-
-let playerChoice = gethumanChoice ()
-
-
-console.log(userName + " choses " + playerChoice + ".")
-
-function getcomputerChoice() {
-    let ranNum = (Math.random())
-
-    if (ranNum <= 0.3) {
-        return "rock"
-    }
-
-    else if (ranNum <= 0.6) {
-        return "paper"
-    }
-
-    else if (ranNum >= 0.6) {
-        return "scissors"
-    }
-}
-
-let computerChoice = getcomputerChoice()
-console.log("Computer choses " + computerChoice + ".")
-
-function round() {
-    if (computerChoice == playerChoice)
-        return("Draw for this round")
-    else if (playerChoice == "rock" && computerChoice == "scissors" || playerChoice == "paper" && computerChoice == "rock" || playerChoice == "scissors" && computerChoice == "paper"){
-        playerScore ++
-        console.log("player score: " + playerScore)
-        return(userName + " wins this round")
+        if (userInput == "rock") {
+            return "rock"
         }
-    //else if (computerChoice == "rock" && playerChoice == "scissors" || computerChoice == "paper" && playerChoice == "rock" || computerChoice == "scissors" && playerChoice == "paper"){
-    else {  
-        computerScore ++
-        console.log("computer score: " + computerScore)
-        return("Computer wins this round")
+
+        else if (userInput == "paper") {
+            return "paper"
+
+        }
+        else if (userInput == "scissors") {
+            return "scissors"
+        }
+
+        else {
+            return "error"
+        }
+
     }
-}
-roundResult = round()
-console.log(roundResult)
+
+    let playerChoice = gethumanChoice()
+
+
+    console.log(userName + " choses " + playerChoice + ".")
+
+    function getcomputerChoice() {
+        let ranNum = (Math.random())
+
+        if (ranNum <= 0.3) {
+            return "rock"
+        }
+
+        else if (ranNum <= 0.6) {
+            return "paper"
+        }
+
+        else if (ranNum >= 0.6) {
+            return "scissors"
+        }
+    }
+
+    let computerChoice = getcomputerChoice()
+    console.log("Computer choses " + computerChoice + ".")
+
+    function round() {
+        if (computerChoice == playerChoice) {
+            return ("Draw for this round")
+            console.log("player score: " + playerScore)
+            console.log("computer score: " + computerScore)
+        }
+        else if (playerChoice == "rock" && computerChoice == "scissors" || playerChoice == "paper" && computerChoice == "rock" || playerChoice == "scissors" && computerChoice == "paper") {
+            playerScore++
+            console.log("player score: " + playerScore)
+            console.log("computer score: " + computerScore)
+            return (userName + " wins this round")
+        }
+        else {
+            computerScore++
+            console.log("player score: " + playerScore)
+            console.log("computer score: " + computerScore)
+            return ("Computer wins this round")
+        }
+    }
+    roundResult = round()
+    console.log(roundResult)
 }
 
-for (let i = 0; i < roundNumbers; i++){
+for (let i = 0; i < roundNumbers; i++) {
     fullGame()
 }
 
-console.log("computer final score: " + computerScore)
 console.log("player final score: " + playerScore)
-    
+console.log("computer final score: " + computerScore)
 
 if (playerScore > computerScore) {
-    console.log ("player wins!")
+    console.log("player wins!")
     console.log("game over")
 }
 else if (playerScore < computerScore) {
