f5cfdb4cbf61648bd05e397b5e831a90485f24f2
marcebrit <248681894+marcebrit@users.noreply.github.com>
2025-12-29 18:16:04 -0300

    UI added to this branch

--- script.js
+++ script.js
@@ -1,100 +1,94 @@
-let userName = (prompt("Insert your username"))
-let roundNumbers = (prompt("Insert round numbers (5 maximum)"))
 
-
+let userName = ("Player")
 let playerScore = 0
 let computerScore = 0
 
+const divLog = document.getElementById("log")
+const paraPlayer = document.createElement("p");
+const paraComputer = document.createElement("p")
 
-function fullGame() {
+const divScore = document.getElementById("score")
+const paraScorePlayer = document.createElement("p")
+const paraScoreComputer = document.createElement("p")
+const roundResult = document.createElement("p")
 
-    function gethumanChoice() {
-        let userInput = (prompt("rock, paper or scissors"))
+const resultLog = document.getElementById("game-result")
+const gameResult = document.createElement("p")
 
-        if (userInput == "rock") {
-            return "rock"
-        }
+const buttons = document.querySelectorAll("button")
 
-        else if (userInput == "paper") {
-            return "paper"
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        round(button.id)
+    })
+})
 
-        }
-        else if (userInput == "scissors") {
-            return "scissors"
-        }
+function getcomputerChoice() {
+    let ranNum = (Math.random())
 
-        else {
-            return "error"
-        }
-
+    if (ranNum <= 0.3) {
+        return "rock"
     }
 
-    let playerChoice = gethumanChoice()
-
-
-    console.log(userName + " choses " + playerChoice + ".")
-
-    function getcomputerChoice() {
-        let ranNum = (Math.random())
-
-        if (ranNum <= 0.3) {
-            return "rock"
-        }
-
-        else if (ranNum <= 0.6) {
-            return "paper"
-        }
-
-        else if (ranNum >= 0.6) {
-            return "scissors"
-        }
+    else if (ranNum <= 0.6) {
+        return "paper"
     }
+
+    else if (ranNum >= 0.6) {
+        return "scissors"
+    }
+}
+
+function round(playerChoice) {
 
     let computerChoice = getcomputerChoice()
-    console.log("Computer choses " + computerChoice + ".")
+    paraPlayer.textContent = ("Player choses " + playerChoice + ".")
+    paraComputer.textContent = ("Computer choses " + computerChoice + ".")
+    divLog.appendChild(paraPlayer)
+    divLog.appendChild(paraComputer)
 
-    function round() {
-        if (computerChoice == playerChoice) {
-            return ("Draw for this round")
-            console.log("player score: " + playerScore)
-            console.log("computer score: " + computerScore)
+    if (computerChoice == playerChoice) {
+        paraScorePlayer.textContent = ("player score: " + playerScore)
+        paraScoreComputer.textContent = ("computer score: " + computerScore)
+        divScore.appendChild(paraScorePlayer)
+        divScore.appendChild(paraScoreComputer)
+        roundResult.textContent = "Draw for this round"
+        divScore.appendChild(roundResult)
+
+    }
+    else if (playerChoice == "rock" && computerChoice == "scissors" ||
+        playerChoice == "paper" && computerChoice == "rock" ||
+        playerChoice == "scissors" && computerChoice == "paper") {
+        playerScore++
+        paraScorePlayer.textContent = ("player score: " + playerScore)
+        paraScoreComputer.textContent = ("computer score: " + computerScore)
+        divScore.appendChild(paraScorePlayer)
+        divScore.appendChild(paraScoreComputer)
+        roundResult.textContent = `${userName} win this round`
+        divScore.appendChild(roundResult)
+    }
+    else {
+        computerScore++
+        paraScorePlayer.textContent = ("player score: " + playerScore)
+        paraScoreComputer.textContent = ("computer score: " + computerScore)
+        divScore.appendChild(paraScorePlayer)
+        divScore.appendChild(paraScoreComputer)
+        roundResult.textContent = `Computer win this round`
+        divScore.appendChild(roundResult)
+    }
+
+    if (playerScore >= 5 || computerScore >= 5) {
+        if (playerScore > computerScore) {
+            gameResult.textContent = `${userName} wins!`
+
         }
-        else if (playerChoice == "rock" && computerChoice == "scissors" ||
-            playerChoice == "paper" && computerChoice == "rock" ||
-            playerChoice == "scissors" && computerChoice == "paper") {
-            playerScore++
-            console.log("player score: " + playerScore)
-            console.log("computer score: " + computerScore)
-            return (userName + " wins this round")
+        else if (playerScore < computerScore) {
+            gameResult.textContent = `Computer wins!`
         }
         else {
-            computerScore++
-            console.log("player score: " + playerScore)
-            console.log("computer score: " + computerScore)
-            return ("Computer wins this round")
+            gameResult.textContent = `It's a tie`
         }
+        resultLog.appendChild(gameResult)
+        buttons.forEach(button => button.disabled = true)
     }
-    roundResult = round()
-    console.log(roundResult)
 }
-
-for (let i = 0; i < roundNumbers; i++) {
-    fullGame()
-}
-
-console.log("player final score: " + playerScore)
-console.log("computer final score: " + computerScore)
-
-if (playerScore > computerScore) {
-    console.log("player wins!")
-    console.log("game over")
-}
-else if (playerScore < computerScore) {
-    console.log("computer wins!")
-    console.log("game over")
-}
-else {
-    console.log("It's a draw!")
-    console.log("game over")
-}
-
